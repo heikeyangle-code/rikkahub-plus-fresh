5f7569b0a4cafe5d8a8667ed3d9d22361da1e9a6
heikeyangle <heikeyangle@gmail.com>
2026-05-28 13:59:54 +0800

    upgrade AGP from 8.9.2 to 9.2.1 (support compileSdk=37)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,12 +15,14 @@ plugins {
 
 android {
     namespace = "me.rerere.rikkahub"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "me.rerere.rikkahub"
         minSdk = 26
-        targetSdk = 37
+        multiDexEnabled = true
+        multiDexKeepProguard = file("multidex-keep.pro")
+        targetSdk = 35
         versionCode = 162
         versionName = "2.2.6"
 
